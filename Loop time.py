e06c4df50e97f86d365a366bfa8eac62113969ca
DivyeshYadav <prabhakaryadav621@gmail.com>
2025-01-23 15:29:35 +0545

    loop time

---
+++ Loop time.py
@@ -0,0 +1,11 @@
+def myfunction(n):
+    for i in range(0,n+1):
+        print("First loop")
+
+    j=1
+    while(j<=m+1):
+        print("Second Loop",j)
+        j=j*2
+
+    for i in range(0,100):
+        print("Third Loop")        
